f3ea4c2264f40f4e120a8385b217b9f1b30aba17
Miguel Gamboa <miguelgamboa@outlook.com>
2023-05-10 22:37:14 +0100

    lesson 27

--- lesson26-sequences/src/main/kotlin/pt/isel/Queries.kt
+++ lesson26-sequences/src/main/kotlin/pt/isel/Queries.kt
@@ -1,3 +1,6 @@
+import java.util.Optional
+import java.util.OptionalDouble
+
 /**
  * Receiver - Iterable<T>
  * T - type parameter (i.e. generic parameter)
@@ -16,10 +19,81 @@ fun <T, R> Iterable<T>.convert(transform: (T) -> R) : Iterable<R> {
     return destination
 }
 
+fun <T, R> Sequence<T>.convert(transform: (T) -> R) : Sequence<R> {
+    return sequence {
+        val iter = this@convert.iterator()
+        while(iter.hasNext()) {
+            val item = iter.next()
+            yield(transform(item))
+        }
+    }
+    /*
+    return object : Sequence<R> {
+        override fun iterator(): Iterator<R> {
+            return SequenceConvert(this@convert, transform)
+        }
+    }
+    */
+}
+
+class SequenceConvert<T, R>(
+    source: Sequence<T>,
+    val transform: (T) -> R)
+    : Iterator<R>
+{
+    val iterator = source.iterator()
+    override fun hasNext() = iterator.hasNext()
+    override fun next(): R = transform(iterator.next())
+}
+
 fun <T> Iterable<T>.where(predicate: (T) -> Boolean) : Iterable<T> {
     val destination = mutableListOf<T>()
     for (item in this)
         if(predicate(item))
         destination.add(item)
     return destination
+}
+
+fun <T> Sequence<T>.where(predicate: (T) -> Boolean) : Sequence<T> {
+    return sequence {
+        for (item in this@where)
+            if(predicate(item))
+                yield(item)
+    }
+    /*
+    return object : Sequence<T> {
+        override fun iterator(): Iterator<T> {
+            return SequenceFilter(this@where, predicate)
+        }
+    }
+    */
+}
+
+class SequenceFilter<T>(source: Sequence<T>, val predicate: (T) -> Boolean) : Iterator<T> {
+    val iterator = source.iterator()
+    var current = advance()
+
+    private fun advance() : Optional<T> {
+        while (iterator.hasNext()) {
+            val item = iterator.next()
+            if(predicate(item))
+                return Optional.of(item)
+        }
+        return Optional.empty()
+    }
+
+    override fun hasNext() = current.isPresent
+
+    override fun next(): T {
+        val item = current.get()
+        current = advance()
+        return item
+    }
+}
+
+
+fun foo() = sequence<Int> {
+    yield(7)
+    yield(6)
+    yield(5)
 }
--- lesson26-sequences/src/test/kotlin/pt/isel/QueriesTest.kt
+++ lesson26-sequences/src/test/kotlin/pt/isel/QueriesTest.kt
@@ -59,15 +59,33 @@ class QueriesTest {
     }
 
     @Test
-    fun `Sequence extensions test map filter first`() {
+    fun `Lazy Queries count iterations`() {
+        var count = 0
         val name = File(lae2022uri)
             .readLines()
             .asSequence()
-            .map { println("map 1 $it"); it.toStudent() }
-            .filter {  println("filter 2 $it");it.nr > 47000 }
-            .map {  println("map 3 $it");it.name.split(" ").last() }
-            .filter {  println("filter 4$it");it.startsWith("A") }
+            .convert { count++; it.toStudent() }
+            .where { count++; it.nr > 47000 }
+            .convert { count++; it.name.split(" ").last() }
+            .where { count++; it.startsWith("A") }
+            .iterator().next()
+        // assertEquals(146, count)
+        assertEquals(22, count)
+        assertEquals("Almeida", name)
+    }
+
+    @Test
+    fun `Sequence extensions test map filter first`() {
+        var count = 0
+        val name = File(lae2022uri)
+            .readLines()
+            .asSequence()
+            .map { count++; it.toStudent()}
+            .filter { count++; it.nr > 47000 }
+            .map { count++; it.name.split(" ").last() }
+            .filter { count++; it.startsWith("A") }
             .first()
+        assertEquals(22, count)
         assertEquals("Almeida", name)
     }
 
